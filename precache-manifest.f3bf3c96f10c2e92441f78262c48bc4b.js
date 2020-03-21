2a45ae9ba9b928bd4443ae2ba2fe10df335a257a
Raja <draja@informatica.com>
2020-03-22 00:29:07 +0530

    Updates

---
+++ precache-manifest.f3bf3c96f10c2e92441f78262c48bc4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88633004b7172ea4e5dc66771533d54c",
+    "url": "/ToDo/index.html"
+  },
+  {
+    "revision": "f41a815d8427cc4a2920",
+    "url": "/ToDo/static/css/main.cab5cf94.chunk.css"
+  },
+  {
+    "revision": "a2e084e8634d451817d5",
+    "url": "/ToDo/static/js/2.50f5848d.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/ToDo/static/js/2.50f5848d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f41a815d8427cc4a2920",
+    "url": "/ToDo/static/js/main.d2b7aae7.chunk.js"
+  },
+  {
+    "revision": "67f65cb31672102d04e8",
+    "url": "/ToDo/static/js/runtime-main.56f6a4a0.js"
+  }
+]);
