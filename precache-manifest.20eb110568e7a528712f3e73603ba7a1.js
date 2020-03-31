9c436b9bca7f2d96ae49595688fbe39ec373ee06
Raja <draja@informatica.com>
2020-03-31 21:39:08 +0530

    Updates

---
+++ precache-manifest.20eb110568e7a528712f3e73603ba7a1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17cded71eeccdb7530b889d1fa47112a",
+    "url": "/InfaKart/index.html"
+  },
+  {
+    "revision": "33ff58d38570578b8234",
+    "url": "/InfaKart/static/css/main.9d296d50.chunk.css"
+  },
+  {
+    "revision": "671b292f2108bdeb73b3",
+    "url": "/InfaKart/static/js/2.89838434.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/InfaKart/static/js/2.89838434.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33ff58d38570578b8234",
+    "url": "/InfaKart/static/js/main.749b3924.chunk.js"
+  },
+  {
+    "revision": "14bcb4e1a4c171ab4a69",
+    "url": "/InfaKart/static/js/runtime-main.e3513e68.js"
+  },
+  {
+    "revision": "4951079796bb692907a51ab017bf42ca",
+    "url": "/InfaKart/static/media/loading.49510797.gif"
+  },
+  {
+    "revision": "25dea1c62bf86b14feb110bbf34d8f46",
+    "url": "/InfaKart/static/media/placeholder.25dea1c6.jpg"
+  }
+]);
